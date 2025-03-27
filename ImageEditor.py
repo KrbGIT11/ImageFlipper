80e2b1dd4cd539d891d7c4b1b11a7412162f1563
KrXCode <krbcrisostomo@gmail.com>
2025-03-28 06:26:34 +0800

    Update ImageEditor.py

--- ImageEditor.py
+++ ImageEditor.py
@@ -133,5 +133,5 @@ def save_images():
             f"<a href='/'>Go Back</a>")
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
